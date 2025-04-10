de7f84a7ac72afef82c210f8c4c354d96761357d
Rahul <rahul0987654321dv@gmail.com>
2025-04-10 21:24:02 +0530

    Added Delete Function

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
-import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
 
@@ -36,6 +36,20 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+    
+@app.route('/delete/<id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        
+        return redirect('/')
+    
+    except:
+        return "There was an Issue in deleting your task"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
